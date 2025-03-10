14f54be44d05731a8a5ed252617ff32109f7d412
a26lopez <alejandro26lopeztorres@gmail.com>
2025-03-10 20:37:59 +0100

    Feat: Login screen and login viewModel

---
+++ match4padel-staff/match4padel-staff/ViewModel/LoginViewModel.cs
@@ -0,0 +1,64 @@
+﻿using match4padel_staff.ViewModel;
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace match4padel_staff.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class LoginViewModel : ViewModelBase
+    {
+        //Fields
+        public string Username { get; set; }
+        public SecureString Password { get; set; }
+        public string ErrorMessage { get; set; }
+        public bool IsViewVisible { get; set; } = true;
+
+
+        //Commands
+        public ICommand LoginCommand { get; }
+        public ICommand RememberPassword { get; }
+
+
+        //Constructor
+        public LoginViewModel()
+        {
+            LoginCommand = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLoginCommand);
+            RememberPassword = new ViewModelCommand(p => ExecuteRememberPasswordCommand("", ""));
+        }
+
+        private void ExecuteRememberPasswordCommand(string username, string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLoginCommand(object obj)
+        {
+            bool validData;
+            if (string.IsNullOrEmpty(Username) ||
+                Username.Length < 3 ||
+                Password == null ||
+                Password.Length < 3)
+            {
+                validData = false;
+            }
+            else
+            {
+                validData = true;
+            }
+            return validData;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
---
+++ match4padel-staff/match4padel-staff/ViewModel/ViewModelBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using PropertyChanged;
+
+namespace match4padel_staff.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public abstract class ViewModelBase
+    {
+    }
+}
